0d1d9e76bd14b138d02061864f54d24ec37288b0
YZR0009 <yzr0009@tigermail.auburn.edu>
2018-09-21 10:54:19 -0500

    Change port number based on requirement

--- client12.c
+++ client12.c
@@ -15,7 +15,7 @@
 
 #include <arpa/inet.h>
 
-#define PORT "3498" // the port client will be connecting to 
+#define PORT "10010" // the port client will be connecting to 
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once 
 
